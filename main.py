0eb0e968b47eb980e202f853bc1a876ce30f97f9
Mavourn3en <123911050@qq.com>
2020-03-15 02:55:11 -0400

    Create main.py
    first version that satifies the requirments from OA pdf without corner cases considered.

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+s = sys.argv
+s1 = s[1]
+s2 = s[2]
+m = len(s1)
+n = len(s2)
+if m != n:
+    print(False)
+else:
+    dic = {}
+    res = True
+    for i in range(n):
+        if dic.get(s1[i]) == None:
+            dic[s1[i]] = s2[i]
+        else:
+            if dic[s1[i]] != s2[i]:
+                res = False
+    print(res)
